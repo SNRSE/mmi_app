985f4790bb005d44b1d647b1c734982080e522f0
Candlelight-video <38672614+Candlelight-video@users.noreply.github.com>
2019-04-16 20:17:14 +0200

    Update Backend.php

--- Backend.php
+++ Backend.php
@@ -1,19 +1,26 @@
 <?php
  $function = $_POST["funktion"];
-  $data = $_POST["data"];
+ $data = $_POST["data"];
+$auth = true;
 if($auth == true) {
- if (function_exists($function_name)){
+ if (function_exists($function)== true){
+	 echo("hi");
  $response = $function($data);
  } else{
+	 echo("false");
 	 $response = array (
 						"sucess" => false,
-						"error"  => "Funktion does not exist");
+						"error"  => "Funktion does not exist");}
 }else{
 	$response = array (
 						"sucess" => false,
 						"error"  => "Nicht authentifiziert");
 }
 // codieren der Daten
-print_r(json_encode($response));
- 
+echo(json_encode($response));
+ function get_version(){
+	 $response = array (
+						"sucess" => true,
+						"version"  => "0.1");
+ }
 ?>
