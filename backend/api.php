ba09c96e91ba1cef649f5ed96f889a764c45928e
Candlelight-video <38672614+Candlelight-video@users.noreply.github.com>
2019-04-17 23:25:48 +0200

    added first test function

--- backend/api.php
+++ backend/api.php
@@ -16,10 +16,24 @@ if($auth == true) {
 }
 // codieren der Daten
 echo(json_encode($response));
+
+#Functionen :
  function get_version($fdata){
 	 $response = array (
 						"sucess" => true,
 						"version"  => "0.1");
+     return($response);
+ }
+ function get_userdata($fdata){
+	 $response = array (
+						"sucess" => true,
+                        "id"  => "1",
+						"username"  => "Max",
+                        "name"  => "Max Musterman",
+                        "alter"  => "13",
+                        "ort"  => "Metzingen",
+                        "punkte"  => "120"
+     );
      return($response);
  }
 ?>
