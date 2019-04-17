17c3352731ef2da98c12d0e91c26c31bd754be77
Candlelight-video <38672614+Candlelight-video@users.noreply.github.com>
2019-04-17 23:28:39 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-function get_server_data(call_function,data){
+function call_server(call_function,data){
     var daten = {"function": call_function,"data":data};
     $.ajax({
         url: "/backend/api.php",
@@ -10,4 +10,13 @@ function get_server_data(call_function,data){
 function response_server_data(data){
     var response = $.parseJSON(data);
     console.log(response);
+}
+function call_server(call_function,data){
+    var daten = {"function": call_function,"data":data};
+    $.ajax({
+        url: "/backend/api.php",
+		type: "POST",
+		data:daten,
+        success: function(data) { return(data); }
+   });
 }
