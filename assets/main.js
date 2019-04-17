80b55d980531ffcd62f8225948810729b78840ca
Candlelight-video <38672614+Candlelight-video@users.noreply.github.com>
2019-04-17 23:38:53 +0200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,13 @@
-function call_server(call_function,data){
+function get_data(call_function,data,response_func){
     var daten = {"function": call_function,"data":data};
     $.ajax({
         url: "/backend/api.php",
 		type: "POST",
 		data:daten,
-        success: function(data) { response_server_data(data); }
+        success: function(data) { response_func(data); }
    });
 }
-function response_server_data(data){
+function log_response(data){
     var response = $.parseJSON(data);
     console.log(response);
-}
-function call_server_demo(call_function,data){
-    var daten = {"function": call_function,"data":data};
-    $.ajax({
-        url: "/backend/api.php",
-		type: "POST",
-		data:daten,
-        success: function(data) { alert(data); }
-   });
 }
