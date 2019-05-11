eb3888a748952c638d09baa5a78c4c819b80c4fb
Jonas Dautel <jonasdautel@outlook.de>
2019-05-11 22:07:54 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,13 +20,9 @@ var pageid = 0
 placeholder["username"] = " Jonas 2";
 
 function isMobile() {
- try {
-    if(/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)) {
-     return true;
-    };
-    return false;
- } catch(e){ console.log("Error in isMobile"); return false; }
-}
+ return true;
+ 
+ }
 function GetURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
